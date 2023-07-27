2384573d970304d3e3bf7ab7ecc0d6e15528c9d8
kavi-ivykids <136069340+kavi-ivykids@users.noreply.github.com>
2023-07-27 21:16:25 +0530

    node js and server setup successfull

--- backend/config/database.js
+++ backend/config/database.js
@@ -2,16 +2,10 @@ const mongoose          = require("mongoose");
 const { MONGO_URI }     = process.env;
 const AppStrings        = require("../constants/app_strings");
 
-
 // Connecting to the database
 exports.connect = () => {
     mongoose
-      .connect(MONGO_URI, {
-        useNewUrlParser: true,
-        useUnifiedTopology: true,
-        useCreateIndex: true,
-        useFindAndModify: false,
-      })
+      .connect(MONGO_URI)
       .then(() => {
         console.log(AppStrings.database_connection_success);
       })
--- backend/constants/app_strings.js
+++ backend/constants/app_strings.js
@@ -1,6 +1,6 @@
 const AppStrings = {
     database_connection_success : "Successfully connected to the database",
-    database_connection_failure : "database connection failed. exiting now...",
+    database_connection_failure : "database connection failed exiting now...",
     server_port                 : "Server running on port",
 }
 
