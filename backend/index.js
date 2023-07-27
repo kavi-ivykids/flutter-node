22d2a580f8958802bbccce4e0de22fc4fe594bb8
kavi-ivykids <136069340+kavi-ivykids@users.noreply.github.com>
2023-07-27 23:40:55 +0530

    app errors and users schema

--- backend/index.js
+++ backend/index.js
@@ -1,13 +1,12 @@
-const http              = require("http");
-const app               = require("./app");
-const server            = http.createServer(app);
-const AppStrings        = require("./constants/app_strings");
+const http = require('http')
+const app = require('./app')
+const server = http.createServer(app)
+const AppStrings = require('./constants/app_strings')
+const { API_PORT } = process.env
 
+const port = process.env.PORT || API_PORT
 
-const { API_PORT }      = process.env;
-const port              = process.env.PORT || API_PORT;
-
-// server listening 
+// server listening
 server.listen(port, () => {
-  console.log(`${AppStrings.server_port} ${port}`);
-});
+  console.log(`${AppStrings.server_port} ${port}`)
+})
---
+++ backend/model/user.js
@@ -0,0 +1,10 @@
+const mongoose = require("mongoose");
+
+const UserSchema = new mongoose.Schema({
+  username: { type: String, unique: true, required: true },
+  email: { type: String, unique: true, required: true },
+  password: { type: String, unique: true, required: true },
+  token: { type: String },
+});
+
+module.exports = mongoose.model("user", userSchema);
