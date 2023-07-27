0ad19f7561c60ec73d1a372c73f0dc62b8f4312f
kavi-ivykids <136069340+kavi-ivykids@users.noreply.github.com>
2023-07-27 21:05:58 +0530

    re-commit

---
+++ backend/constants/app_strings.js
@@ -0,0 +1,7 @@
+const AppStrings = {
+    database_connection_success : "Successfully connected to the database",
+    database_connection_failure : "database connection failed. exiting now...",
+    server_port                 : "Server running on port",
+}
+
+module.exports = AppStrings;
